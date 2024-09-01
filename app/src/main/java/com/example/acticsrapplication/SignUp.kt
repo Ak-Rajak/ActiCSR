c879559077331ad1c197aa1a1cbf4d5ee483c311
Ak-Rajak <atulkumar.coderguys@gmail.com>
2024-09-02 02:43:15 +0530

    Work on the Signup file in actiCSR

--- app/src/main/java/com/example/acticsrapplication/SignUp.kt
+++ app/src/main/java/com/example/acticsrapplication/SignUp.kt
@@ -2,6 +2,7 @@ package com.example.acticsrapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,34 @@ import androidx.core.view.WindowInsetsCompat
 
 class SignUp : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+
+    private lateinit var nameEditText: TextInputEditText
+    private lateinit var emailEditText: TextInputEditText
+    private lateinit var passwordEditText: TextInputEditText
+    private lateinit var confirmPasswordEditText: TextInputEditText
+    private lateinit var signUpButton: MaterialButton
+    private lateinit var goToLoginTextView: TextView
+
+
+    private fun validateInput(name: String, email: String, password: String, confirmPassword: String): Boolean {
+        if (TextUtils.isEmpty(name)){
+            nameEditText.error = "Name is required"
+        }
+
+        if (TextUtils.isEmpty(email)){
+            nameEditText.error = "Email is required"
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            nameEditText.error = "Password is required"
+        }
+
+        if (TextUtils.isEmpty(confirmPassword)){
+            nameEditText.error = "Password must be at least 6 character long"
+            return false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +46,24 @@ class SignUp : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        nameEditText = findViewById<>(R.id.nameEditText)
+        emailEditText = findViewById<>(R.id.emailEditText)
+        passwordEditText = findViewById<>(R.id.passwordEditText)
+        confirmPasswordEditText = findViewById<>(R.id.confirmPasswordEditText)
+        signUpButton = findViewById<>(R.id.signUpButton)
+        goToLoginTextView = findViewById<>(R.id.go_login_screen)
+
+        signUpButton.setOnClickLister {
+            val name = nameEditText.text.toString().trim()
+            val email = nameEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+            val confirmPassword = confirmPasswordEditText.text.toString().trim()
+
+            if (validateInput(name, email , password , confirmPassword)) {
+                signup(name,email,password)
+            }
         }
     }
 }
