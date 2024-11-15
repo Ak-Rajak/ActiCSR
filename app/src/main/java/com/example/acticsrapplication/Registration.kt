301b4b571e905b97a8b64fc21fb4d8091a87d1d1
Sayan Ash <sayanash347200@gmail.com>
2024-11-15 15:04:07 +0530

    Updated the Registration class

--- app/src/main/java/com/example/acticsrapplication/Registration.kt
+++ app/src/main/java/com/example/acticsrapplication/Registration.kt
@@ -1,7 +1,7 @@
 package com.example.acticsrapplication
 
 data class Registration(
-    val studentName: String,
-    val studentId: String,
-    val eventTitle: String
+    val name: String,
+    val email: String
 )
+
